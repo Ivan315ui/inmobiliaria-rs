a7ce90dbf34dd80bed34f58e69af01f1286a3dc3
Ivan315ui <ivanemanuel315@gmail.com>
2020-09-06 16:25:41 -0300

    ahora sí está terminada con todo lo que pidio

--- js/master.js
+++ js/master.js
@@ -69,10 +69,10 @@ function displayMobileMenu() {
 setInterval(() => {
     let gridItems = document.getElementsByClassName('grid-item');
     gridItems = [].slice.call(gridItems);
-    let height = gridItems[4].offsetWidth;
+    let height = gridItems[2].offsetWidth;
     if (document.querySelector('html').offsetWidth >= 768) {
         document.querySelector('div.grid-container').style.gridTemplateRows = `repeat(2, ${height + "px"})`;
     } else {
-        document.querySelector('div.grid-container').style.gridTemplateRows = `repeat(5, ${height / 2 + "px"})`;
+        document.querySelector('div.grid-container').style.gridTemplateRows = `repeat(4, ${height / 2 + "px"})`;
     }
 }, 500);
