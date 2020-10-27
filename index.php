fdb719e8183486793ce00c4695664c00ab101831
jereet <55718356+jereet@users.noreply.github.com>
2020-10-27 19:36:26 -0300

    Destacados aleatorios listo (Voy a terminarlo el jueves)

--- index.php
+++ index.php
@@ -19,6 +19,20 @@
 
 <body>
 	<?php require('templates/header.html') ?>
+	<?php
+
+	require_once("conexion.php");
+
+	if (empty($_GET)) {
+		
+		//consulta que solicita los datos de las propiedades y cambia su id de tipo por el nombre del tipo de propiedad que es (INNER JOIN)
+		$consulta = $conexionBD->prepare("SELECT propiedades.ID_Propiedad, propiedades.Título, propiedades.Dirección, tipos_propiedades.Nombre_Tipo AS Tipo, propiedades.Piso, propiedades.Departamento, propiedades.Descripción, propiedades.Localidad, propiedades.Categoría FROM propiedades INNER JOIN tipos_propiedades ON propiedades.ID_Tipo = tipos_propiedades.ID_Tipo WHERE Categoría!='No disponible'");
+		$consulta->execute();
+
+		$resultados = $consulta->fetchAll();
+	}
+
+	?>
 	<div class="slider">
 		<div class="slide"></div>
 		<div class="slide"></div>
@@ -68,9 +82,39 @@
 	</section>
 	<section id="destacados">
 		<h2>Destacados</h2>
+		<?php 
+				$lenght = count($resultados);
+
+				$random = rand(1, $lenght);
+
+				$array = [];
+				for($i = 0; $i < 5; $i++){
+					$random = rand(1, $lenght);
+					if (!in_array($random, $array)) {
+						$random = $random;
+					}else{
+						$random = rand(1, $lenght);
+					}
+					foreach($resultados as $resultado => $propiedad){
+						$propiedad['ID_Propiedad'] = $random;
+					}
+					$array[$i] = $propiedad['ID_Propiedad'];
+					
+				}
+				echo $array[0];
+				echo "<br>";
+				echo $array[1];
+				echo "<br>";
+				echo $array[2];
+				echo "<br>";
+				echo $array[3];
+				echo "<br>";
+				echo $array[4];
+				?>
 		<div class="container">
 			<div class="grid-wrapper">
 				<div class="grid-item">
+				
 					<a href="">
 						<img src="imgs/Casa-pago-chico.jpg">
 						<div class="desc">
@@ -130,6 +174,7 @@
 						<h4>Ver más <i class='bx bx-chevron-right'></i></h4>
 					</a>
 				</div>
+				
 				<button class="slide-buttons left">
 					<i class="fas fa-caret-left"></i>
 				</button>
@@ -139,6 +184,7 @@
 			</div>
 			<a href="propiedades.php" class="button">Ver el catálogo completo</a>
 		</div>
+			
 	</section>
 	<section id="contacto">
 		<div class="banner">
