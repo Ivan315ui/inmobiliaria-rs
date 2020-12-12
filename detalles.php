580bd6a9b13d6ef172ef4405cb8583c046d9cf15
Jeremias Toranzo <55718356+jereet@users.noreply.github.com>
2020-12-11 23:53:39 -0300

    Corrección de un mini error previo

--- detalles.php
+++ detalles.php
@@ -94,10 +94,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                     <a href=""><i class="fas fa-phone-alt"></i>Llamar por teléfono</a>
                 </form>
             </div>
-		<div class="mapa">
-			<?php require_once('map.php'); ?>
-		</div>
         </div>
+	<div class="mapa">
+		<?php require_once('map.php'); ?>
+	</div>
     </div>
     <?php require_once 'templates/footer.html'; ?>
 </body>
